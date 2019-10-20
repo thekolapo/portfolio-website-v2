5e32286c8bc2fbd16a1681f6e6b6f9f66139b998
Kolapo Oni <onikolapo@gmail.com>
2019-10-20 13:01:16 +0100

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Kolapo Oni',
+    title: 'Kolapo Oni - Interactive Designer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
